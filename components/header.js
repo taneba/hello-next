d0a3f5d49edf5f1350e675a8688b5aa870d1b60b
ynakamura <ynakamura@r.recruit.co.jp>
2017-09-28 21:58:05 +0900

    add header

---
+++ components/header.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import styled from 'styled-components'
+import A from './a'
+
+const Header = () => (
+  <Wrapper>
+    <Link href="/">
+      <A >Home</A>
+    </Link>
+    <Link href="/about">
+      <A >About</A>
+    </Link>
+  </Wrapper>
+)
+
+const Wrapper = styled.div`
+  padding: 12px;
+  border: 2px solid #dddddd;
+`
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import Link from 'next/link'
+import styled from 'styled-components'
+import Header from '../components/header'
+
 const Index = () => (
   <div>
+    <Header />
     <p>Hello Next.js</p>
   </div>
 )
